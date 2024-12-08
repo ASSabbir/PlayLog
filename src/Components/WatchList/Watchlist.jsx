e35b1d5b19c24fb08063c722fe7eba3b73229d85
ASSabbir <atiksabbir1125@gmail.com>
2024-12-08 22:04:32 +0600

    added some section

--- src/Components/WatchList/Watchlist.jsx
+++ src/Components/WatchList/Watchlist.jsx
@@ -1,27 +1,27 @@
-import  { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../Context/MainContext';
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom';
 
 const Watchlist = () => {
-    const { user, watchData,setWatchData } = useContext(AuthContext)
+    const { user, watchData, setWatchData } = useContext(AuthContext)
     console.log(user, watchData)
     const watch = watchData.filter(data => data.addedUser == user.email)
-    
+
 
 
     if (!watchData.length) {
         return <div className="h-screen flex justify-center items-center">
-        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
-            <div className="h-48 rounded-t bg-zinc-800"></div>
-            <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-zinc-700">
-                <div className="w-full h-6 rounded bg-zinc-800"></div>
-                <div className="w-full h-6 rounded bg-zinc-800"></div>
-                <div className="w-3/4 h-6 rounded bg-zinc-800"></div>
+            <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+                <div className="h-48 rounded-t bg-zinc-800"></div>
+                <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-zinc-700">
+                    <div className="w-full h-6 rounded bg-zinc-800"></div>
+                    <div className="w-full h-6 rounded bg-zinc-800"></div>
+                    <div className="w-3/4 h-6 rounded bg-zinc-800"></div>
+                </div>
             </div>
         </div>
-    </div>
-      }
+    }
     if (watch.length == 0) {
         return <div className='h-[calc(100vh-64px)] font-title text-4xl flex-col flex justify-center items-center'>
             <h1>You Didnt Add Any Games</h1>
@@ -41,8 +41,6 @@ const Watchlist = () => {
                                 className="rounded-lg shadow-lg"
                             />
                         </div>
-
-
                         <div className="w-full flex-1  space-y-4 ">
                             <h1 className="text-3xl font-bold">{data.gameTitle}</h1>
                             <p className="text-gray-400 text-sm">
@@ -63,7 +61,7 @@ const Watchlist = () => {
                                     🌟 Top Game
                                 </div>
                             )}
-                            
+
                         </div>
                     </div>)
                 }
